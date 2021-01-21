079c590136ab9c90529658a6b9a34f2b8b421fd6
shilp <36255097+shilp121@users.noreply.github.com>
2021-01-21 16:49:12 +0530

    changes in user.html, dashboard update

---
+++ test/cybercomtest/user.js
@@ -0,0 +1,88 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["password"] = document.getElementById("password").value;
+    formData["birthdate"] = document.getElementById("birthdate").value;
+    formData["age"] = document.getElementById("age").value;
+
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.email;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.password;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.birthdate;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.age;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("birthdate").value = "";
+    document.getElementById("age").value = "";
+
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("password").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("birthdate").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("age").value = selectedRow.cells[4].innerHTML;
+
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.email;
+    selectedRow.cells[2].innerHTML = formData.password;
+    selectedRow.cells[3].innerHTML = formData.birthdate;
+    selectedRow.cells[4].innerHTML = formData.age;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
